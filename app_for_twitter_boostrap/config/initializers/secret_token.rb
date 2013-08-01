865692db5dc9460230fcc69a15c2663706c4ee57
peterprabu <peterprabu@gmail.com>
2013-08-01 16:21:28 +0530

    first commit for twitter bootstrap

---
+++ app_for_twitter_boostrap/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppForTwitterBoostrap::Application.config.secret_token = '9a41c889022371c7a1a6bd38e4f54318cb13a65e84784bc1fa17975feecf2e2aa406493d47f20d7fc30276c679080549832d4a07915f0364b12e97acfd9df13b'
